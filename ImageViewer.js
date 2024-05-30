edd688bccf1b1475d7e61f8b1c39e9663a409692
Jan Iverson Eligio <janiverson.eligio.ctr@us.navy.mil>
2024-05-29 15:28:13 -1000

    Fixed delay when navigating pictures. Looped around when navigating to edges.

--- ImageViewer.js
+++ ImageViewer.js
@@ -2,70 +2,14 @@ define(['qlik', 'text!./index.html', 'css!./index.css'], function (
     qlik,
     template
 ) {
-    function render(layout) {
-        const imageUrls = layout.qHyperCube.qDataPages[0].qMatrix.map(
-            (cell) => cell[0].qText
-        );
-
-        console.log('imageUrls: ', imageUrls);
-
-        if (imageUrls.length > 0) {
-            $('.image-viewer-box').css(
-                'background-image',
-                `url(${imageUrls[0]})`
-            );
-        }
+    function getNumImages(layout) {
+        return (imageUrls = layout.qHyperCube.qDataPages[0].qMatrix.length);
     }
 
-    function changeImage(currentIndex, layout, direction) {
-        const imageUrls = layout.qHyperCube.qDataPages[0].qMatrix.map(
+    function getImageUrls(layout) {
+        return layout.qHyperCube.qDataPages[0].qMatrix.map(
             (cell) => cell[0].qText
         );
-
-        const newIndex = currentIndex + direction;
-
-        if (newIndex >= imageUrls.length || newIndex < 0) {
-            // Don't do anything
-            console.log('newIndex: ', newIndex);
-            console.log('imageUrls.length: ', imageUrls.length);
-            console.log('returning -1');
-            return -1;
-        }
-
-        if (imageUrls.length > 0) {
-            $('.image-viewer-box').css(
-                'background-image',
-                `url(${imageUrls[newIndex]})`
-            );
-
-            console.log('success. changed to url: ', imageUrls[newIndex]);
-
-            return 1;
-        } else {
-            console.log('ERROR change image');
-            console.log('balls');
-            return -1;
-        }
-    }
-
-    function setImages(layout) {
-        // Flush previous images
-        $('.image-wrapper').remove();
-
-        const imageUrls = layout.qHyperCube.qDataPages[0].qMatrix.map(
-            (cell) => cell[0].qText
-        );
-
-        if (imageUrls.length > 0) {
-            const images = imageUrls.map((url) =>
-                $(`
-                <div class="image-wrapper">
-                    <img src="${url}"/>
-                </div>
-            `)
-            );
-            $('.image-viewer-box').append(images);
-        }
     }
 
     return {
@@ -136,12 +80,6 @@ define(['qlik', 'text!./index.html', 'css!./index.css'], function (
             exportData: false,
         },
         paint: function ($element, layout) {
-            console.log('layout changed: ', layout);
-            console.log('new data: ', layout.qHyperCube.qDataPages);
-
-            // render(layout);
-            setImages(layout);
-
             return qlik.Promise.resolve();
         },
         controller: [
@@ -149,21 +87,41 @@ define(['qlik', 'text!./index.html', 'css!./index.css'], function (
             function ($scope) {
                 //add your rendering code here
                 $scope.html = 'Hello World';
-                const layout = $scope.layout;
 
                 $scope.currentIndex = 0;
+                $scope.numImages = getNumImages($scope.layout);
+                $scope.imageUrls = getImageUrls($scope.layout);
+
                 $scope.prevImage = function () {
                     console.log('Previous image');
-                    const result = changeImage($scope.currentIndex, layout, -1);
-                    if (result > 0)
-                        $scope.currentIndex = $scope.currentIndex - 1;
+
+                    let newIndex = $scope.currentIndex - 1;
+                    if (newIndex < 0) {
+                        newIndex = $scope.numImages - 1;
+                    }
+
+                    $scope.currentIndex = newIndex;
                 };
+
                 $scope.nextImage = function () {
                     console.log('Next image');
-                    const result = changeImage($scope.currentIndex, layout, 1);
-                    if (result > 0)
-                        $scope.currentIndex = $scope.currentIndex + 1;
+
+                    let newIndex = $scope.currentIndex + 1;
+
+                    if (newIndex >= $scope.numImages) {
+                        newIndex = 0;
+                    }
+
+                    $scope.currentIndex = newIndex;
                 };
+
+                $scope.component.model.Validated.bind(function () {
+                    console.info('Validated: ', $scope.layout.qHyperCube);
+
+                    $scope.currentIndex = 0;
+                    $scope.numImages = getNumImages($scope.layout);
+                    $scope.imageUrls = getImageUrls($scope.layout);
+                });
             },
         ],
     };
